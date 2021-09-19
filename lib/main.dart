1cfe9f11c2a792a5674bcabdd0d9b99bb221ba61
rosetanya898 <75381362+rosetanya898@users.noreply.github.com>
2021-09-19 11:21:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -42,8 +33,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
  
-  String url = "https://www.googleapis.com/books/v1/volumes?q=%22c++%22&maxResult=1:keyes&key=AIzaSyCqOTAyAN9dJF5WUqFJk-pzQPU9IdO6aaM";
+  String url = "https://www.googleapis.com/books/v1/volumes?q=%22c++%22&maxResult=1:keyes&key=*****************";
  List data = [];  String topic='';  String write1='';  String write2='';
+  
+  // function to make api request
  Future<dynamic> makereq() async {
    http.Response response = await http.get(Uri.parse(url), headers: {'Accept': 'application/json'});
    
@@ -51,9 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
       var extract = json.decode(response.body);
      data = extract['items'];
    });
-   print(topic);
-   print(write1);
-   print(write2);
+  // print(topic);
+  // print(write1);
+  // print(write2);
  }
 
  @override
